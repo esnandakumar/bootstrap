9bbe570a98a5ee9696fb19920711b42d4348d34a
Nanda Kumar <esnandakuma@gmail.com>
2018-02-25 21:45:40 +0200

    Added seperate scripts file, scss support and parallelshell, onchange npm modules

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $("#mycarousel").carousel({interval: 1000});
+
+    $("#carouselButtons").click(function() {
+        if($("#carouselButtons").children("span").hasClass('fa-pause'))
+        {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButtons").children("span").removeClass('fa-pause');
+            $("#carouselButtons").children("span").addClass('fa-play');
+        }
+        else
+        {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButtons").children("span").removeClass('fa-play');
+            $("#carouselButtons").children("span").addClass('fa-pause');
+        }
+        
+
+    });
+
+    $("#carouselPlay").click(function() {
+        $("#mycarousel").carousel('cycle');
+    });
+
+});
